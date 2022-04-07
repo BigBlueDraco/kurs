eba66226846146faccd8ddac373d98b687aca8ad
Dafar <Dafarlog@gmail.com>
2022-04-07 15:27:14 +0300

    AddActivity, spiner

--- app/src/main/java/com/example/kurs/AddActivity.java
+++ app/src/main/java/com/example/kurs/AddActivity.java
@@ -3,12 +3,36 @@ package com.example.kurs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class AddActivity extends AppCompatActivity {
-
+    private  String[] Status ={"Status1", "Status2"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add2);
+
+        ArrayAdapter<String> statusAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, Status);
+        statusAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner spStatus = (Spinner) findViewById(R.id.spStatus);
+        spStatus.setAdapter(statusAdapter);
+        spStatus.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l)
+            {
+               //Toast.makeText(getBaseContext(), getParent().toString(), Toast.LENGTH_SHORT);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 }
